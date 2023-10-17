ab404a4274500a1fa58f0c1a618656ed6e0c9ce3
unknown <tarikzarouali15@gmail.com>
2023-10-17 16:39:06 +0200

    made a fresh start from my clean mvc structure repository

---
+++ app/views/homepages/index.php
@@ -0,0 +1,26 @@
+<?php require APPROOT . '/views/includes/head.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <h1>welkom medewerker</h1>
+
+
+    <a class="btn btn-info" style="width:50rem; font-size:2rem;" href="<?= URLROOT; ?>schoolscontroller/index/1">School</a>
+
+    <?php require APPROOT . '/views/includes/footer.php'; ?>
+
+
+</body>
+
+</html>
